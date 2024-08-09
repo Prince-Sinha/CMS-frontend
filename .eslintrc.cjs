df14a87e9cd78e845e9b1823f2f6acc65f0dcc82
Ankit <ankit@appointy.com>
2024-08-09 08:19:41 +0530

    feat(client): add floor and washroom pages

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { browser: true, es2020: true, DISABLE_ESLINT: true },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
--- src/pages/FloorSection.jsx
+++ src/pages/FloorSection.jsx
@@ -1,29 +1,79 @@
-import { useParams , useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
-import Submit from '../components/Submit'
+import Submit from "../components/Submit";
 import { useEffect, useState } from "react";
 import Header from "../components/Header";
-export default function FloorSection(){
-    const detail = localStorage.getItem('detail');
-    const [floor , setFloor] = useState(JSON.parse(detail));
-    const { Id } = useParams();
-    const navigate = useNavigate();
-    
+import Cookie from "js-cookie";
+export default function FloorSection() {
+  const [floors, setFloors] = useState([]);
+  const { Id } = useParams();
+  const navigate = useNavigate();
 
-    const handleChange = (floor_id)=>{
-        navigate(`/${Id}/${floor_id}`);
-    }
+  useEffect(() => {
+    fetch(
+      `https://backend.ankitkumar143872.workers.dev/api/v1/cleaner/get-floors`,
+      {
+        method: "get",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${Cookie.get("uuid")}`,
+        },
+      }
+    )
+      .then(async (res) => {
+        const json = await res.json();
+        if (!res.ok) {
+          toast.error("Wrong Phone Number or Password", {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+          return;
+        }
+        console.log(json.floors);
+        setFloors(json.floors);
+      })
+      .catch((err) => {
+        toast.error(err, {
+          position: "top-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+        setLoading((prev) => !prev);
+      });
+  }, []);
 
-    return (
-        <>
-          <Header />
-          <div className='container'>
-             {
-                floor.map(el =>{
-                    return <button key={el.id} onClick={()=>handleChange(el.id)} className="btn"> <h1>{el.name}</h1></button>
-                })
-             } 
-          </div>
-        </>
-      )
+  const handleClick = (floor_id) => {
+    console.log("button clicked");
+    navigate(`/${Id}/${floor_id}`);
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        {floors.map((element, index) => {
+          return (
+            <button
+              key={index}
+              onClick={() => handleClick(element.floor.id)}
+              className="btn"
+            >
+              {element.floor.floor_name}
+            </button>
+          );
+        })}
+      </div>
+    </>
+  );
 }
